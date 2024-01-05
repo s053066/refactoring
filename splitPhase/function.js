5a58088f9a423ad7670e83de6675f69c79713647
katsumaro <ituv2@yahoo.co.jp>
2024-01-05 11:39:26 +0900

    中間データ構造を返すメソッドを追加、不要な定数を削除して完成

--- splitPhase/function.js
+++ splitPhase/function.js
@@ -3,30 +3,37 @@
   * @param {Object} product - 商品情報
   * @param {Number} quantity - 数量
   * @param {Object} shippingMethod - 配送方法
-  * @returns {Number} price - 支払い金額
+  * @returns {Number} - 支払い金額
   */ 
 function priceOrder(product, quantity, shippingMethod) {
+  const priceData = calculatePricingData(product, quantity);
+  return applyShipping(priceData, shippingMethod);
+}
+
+/**
+ * @description 価格計算に必要なデータを計算する
+ * @param {Object} product - 商品情報
+ * @param {Number} quantity - 数量
+ * @returns 
+ */
+function calculatePricingData(product, quantity){
   // basePrice(価格)
   const basePrice = product.basePrice * quantity;
   // discount(値引き額)
   const discount = Math.max(quantity - product.discountThreshold, 0) * product.basePrice * product.discountRate;
 
-  const priceData = {basePrice: basePrice, quantity: quantity, discount: discount};
-  const price = applyShipping(priceData, shippingMethod)
-  return price;
+  return {basePrice: basePrice, quantity: quantity, discount: discount};
+
 }
 
 /**
- * @description 配送料を計算する
- * @param {*} basePrice 
- * @param {*} shippingMethod 
- * @param {*} quantity 
- * @param {*} discount 
- * @returns 
+ * @description 注文商品の合計金額を計算する
+ * @param {Object} priceData - 価格計算に必要なデータ
+ * @param {Object} shippingMethod - 配送方法
+ * @returns 注文商品の合計金額
  */
 function applyShipping(priceData, shippingMethod) {
   const shippingPerCase = (priceData.basePrice > shippingMethod.discountThreshold) ? shippingMethod.discountedFee : shippingMethod.feePerCase;
   const shippingCost = priceData.quantity * shippingPerCase;
-  const price = priceData.basePrice - priceData.discount + shippingCost
-  return price;
+  return  priceData.basePrice - priceData.discount + shippingCost;
 }
